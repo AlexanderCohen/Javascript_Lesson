8c9ca92706a3e3c4a80099cefddeb9cb189eeff8
Alexander Cohen <Alexcohen.is@gmail.com>
2018-02-12 11:16:04 +0000

    halfway through first moduel

--- javascript/source.js
+++ javascript/source.js
@@ -1,1 +1,19 @@
 //Use this document for your JavaScript
+
+
+// JS TOOLKIT
+
+//  window.alert('Window popup')       <-- popup window
+//  document.write('Document Writer')  <-- document write will render html to screen
+//  console.log('logs to console.')    <-- sends text to console.
+
+
+//  document.getElementById("canvas")  <-- Identifies the document element by name
+//  canvas.getContext("2d");           <-- Sets the context for the element
+//  .fillStyle() e.g myVar.fillStyle() <-- sets color for element (uses Hex or RGB)
+//  .fillRect(x pos, y pos, H, Wth)    <-- creates a rectangle with position and size
+//  .clearRect
+
+
+
+// requestAnimationFrame               <-- Called many times per second.
